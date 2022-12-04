187fd625318f0a1629fc54aa742f6e4e37c3c8b6
Omar Thamri <omarthamri4@gmail.com>
2022-12-04 21:04:51 +0100

    Travel Discovery 02: ForEach Hashables and System Images

--- TravelDiscovery/ContentView.swift
+++ TravelDiscovery/ContentView.swift
@@ -12,7 +12,91 @@ struct ContentView: View {
         NavigationView {
             ScrollView {
                     DiscoverCategoriesView()
+                    PopularDestinationsView()
+                    PopularRestaurantsView()
+                    TrendingCreatorsView()
             }.navigationTitle("Discover")
+        }
+    }
+}
+
+struct PopularDestinationsView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Popular destinations")
+                    .font(.system(size: 14,weight: .semibold))
+                Spacer()
+                Text("See all")
+                    .font(.system(size: 12,weight: .semibold))
+            }.padding(.horizontal)
+                .padding(.top)
+            ScrollView(.horizontal) {
+                HStack(spacing: 8) {
+                    ForEach(0..<5, id: \.self) { num in
+                        Spacer()
+                            .frame(width: 125, height: 150)
+                            .background(.gray)
+                            .cornerRadius(5)
+                            .shadow(color: .gray, radius: 4, x: 0, y: 2)
+                            .padding(.bottom)
+                    }
+                }.padding(.horizontal)
+            }
+        }
+    }
+}
+
+struct PopularRestaurantsView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Popular places to eat")
+                    .font(.system(size: 14,weight: .semibold))
+                Spacer()
+                Text("See all")
+                    .font(.system(size: 12,weight: .semibold))
+            }.padding(.horizontal)
+                .padding(.top)
+            ScrollView(.horizontal) {
+                HStack(spacing: 8) {
+                    ForEach(0..<5, id: \.self) { num in
+                        Spacer()
+                            .frame(width: 200, height: 64)
+                            .background(.gray)
+                            .cornerRadius(5)
+                            .shadow(color: .gray, radius: 4, x: 0, y: 2)
+                            .padding(.bottom)
+                    }
+                }.padding(.horizontal)
+            }
+        }
+    }
+}
+
+struct TrendingCreatorsView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Trending creators")
+                    .font(.system(size: 14,weight: .semibold))
+                Spacer()
+                Text("See all")
+                    .font(.system(size: 12,weight: .semibold))
+            }.padding(.horizontal)
+                .padding(.top)
+            ScrollView(.horizontal) {
+                HStack(spacing: 8) {
+                    ForEach(0..<15, id: \.self) { num in
+                        Spacer()
+                            .frame(width: 50, height: 50)
+                            .background(.gray)
+                            .cornerRadius(.infinity)
+                            .shadow(color: .gray, radius: 4, x: 0, y: 2)
+                            .padding(.bottom)
+                    }
+                }.padding(.horizontal)
+            }
         }
     }
 }
@@ -23,20 +107,34 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+struct Category: Hashable {
+    let name,imageName: String
+}
+
 struct DiscoverCategoriesView: View {
+    var categories: [Category] = [
+        .init(name: "Art", imageName: "paintpalette.fill"),
+        .init(name: "Sport", imageName: "sportscourt.fill"),
+        .init(name: "Live events", imageName: "music.mic"),
+        .init(name: "Food", imageName: "music.mic"),
+        .init(name: "History", imageName: "music.mic"),
+    ]
     var body: some View {
         ScrollView(.horizontal,showsIndicators: false) {
-        HStack(spacing: 16) {
-            ForEach(0..<5, id: \.self) { num in
-                VStack(spacing: 4) {
-                    Spacer()
-                        .frame(width: 50, height: 50)
+            HStack(alignment: .top, spacing: 14) {
+            ForEach(categories, id: \.self) { category in
+                VStack(spacing: 8) {
+                    Image(systemName: category.imageName)
+                        .font(.system(size: 20))
+                        .foregroundColor(.white)
+                        .frame(width: 64, height: 64)
                         .background(.gray)
                         .cornerRadius(.infinity)
                         .shadow(color: .gray, radius: 4, x: 0, y: 2)
-                    Text("Art")
+                    Text(category.name)
                         .font(.system(size: 12,weight: .semibold))
-                }
+                        .multilineTextAlignment(.center)
+                }.frame(width: 68)
             }
         }.padding(.horizontal)
         }
