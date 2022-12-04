5535a567d2a7102176eb5409e20185d0f3ed46c2
Omar Thamri <omarthamri4@gmail.com>
2022-12-04 01:09:07 +0100

    Initial Commit

---
+++ TravelDiscovery/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TravelDiscovery
+//
+//  Created by omar thamri on 4/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TravelDiscovery/TravelDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiscoveryApp.swift
+//  TravelDiscovery
+//
+//  Created by omar thamri on 4/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
